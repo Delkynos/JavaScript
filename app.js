41cb91f26d43019ee3bdebaa8e6058b16a7ab6cc
Jaime Santa <Santa0779@gmail.com>
2022-02-02 22:40:54 -0600

    Proyecto de javascritp

---
+++ app.js
@@ -0,0 +1,15 @@
+var nombre = "Jaime";
+
+var nintendo ="Ocupa Firma porque";
+
+const Quierenintendo = true;
+
+if(Quierenintendo){
+
+    console.log(nombre + " "+ nintendo + " " + "Si quiere el nintendo");
+
+    
+}else{
+    console.log(nombre + " "+ nintendo + " " + "No quiere el nintendo");
+        
+}
